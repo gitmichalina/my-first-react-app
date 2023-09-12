682d29fc6101d1258121aab4b7d99679c282d5f5
Michalina Maria <michalina.dev@gmail.com>
2023-09-12 17:50:30 +0200

    new todo element on click int he console

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ function App() {
   ]
 
   const addTodo = () => {
-    console.log('Our addTodo button has been clicked!');
+    if(todos.length > 0){
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription:  'New Todo',
+        rowAssigned: 'User Three'
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
   }
 
   return (
